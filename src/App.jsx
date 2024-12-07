7855458353b49c73cd9b360976c5ffceaaf83bfa
uiux@okdollar frontend <uiux@okdollar.com>
2024-12-07 11:15:58 +0630

    first stage

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-400">
+        Hello world!
+      </h1>
+      <Button variant="contained" color="success">Hello world</Button>
+    </>
+  )
+}
+
+export default App
